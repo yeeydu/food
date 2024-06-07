cd658e2caba1670f617180eae1ae9dcaee878f28
yeeysonduarte <portas3@hotmail.com>
2024-06-07 16:01:26 +0100

    fix prisma addin singleton pattern

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,21 @@
 // lib/prisma.js
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+//const prisma = new PrismaClient()
+
+
+declare global {
+    var cachedPrisma: PrismaClient;
+  }
+  
+  let prisma: PrismaClient;
+  if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient();
+  } else {
+    if (!global.cachedPrisma) {
+      global.cachedPrisma = new PrismaClient();
+    }
+    prisma = global.cachedPrisma;
+  }
 
 export default prisma
