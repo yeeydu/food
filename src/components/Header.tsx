f0262b0029e4ac0c88439360c4b08d7fa13973c3
yeeysonduarte <portas3@hotmail.com>
2024-06-13 10:14:04 +0100

    feat: add link and style fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className="flex justify-between pt-6 px-5">
-      <Image src="/food-logo.png" height={30} width={90} alt="logo" />
+      <div className="relative h-[80px] w-[90px]">
+        <Link href={"/"}>
+        <Image src="/food-logo.png" fill alt="logo" className="object-contain" />
+        </Link>
+      </div>
       <Button
         size="icon"
         variant="outline"
